7ad8b69ed69dbf7efae2532cf7c7e91fbf31cde2
Arkena <ads.neuroshok@gmail.com>
2022-06-15 01:27:49 +0200

    [update] add headers

--- xmake.lua
+++ xmake.lua
@@ -1,14 +1,20 @@
 set_policy("build.ccache", false)
 
-add_rules("mode.debug")
+add_rules("mode.debug", "mode.release")
 
 add_requires("nlohmann_json")
 
 set_languages("cxx23")
 
 target("ncs")
-    set_kind("binary")
-    add_files("source/**.cpp")
+    set_kind("$(kind)")
+
+    if "$(kind)" == "binary" then
+        add_files("source/main.cpp")
+    end
+
+    -- add_files("source/ncs/**.cpp")
+    add_headerfiles("include/(**.hpp)")
     -- add_files("module/**.cpp")
 
     add_includedirs(
